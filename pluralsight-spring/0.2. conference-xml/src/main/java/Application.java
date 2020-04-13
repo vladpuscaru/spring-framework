720352f11aea443d1c8589d4b9dc2dfc3f120812
Puscaru <vpuscaru@rovpuscaru01m.atrema.deloitte.com>
2020-04-13 18:37:32 +0300

    beanConfig-xml

--- pluralsight-spring/0.2. conference-xml/src/main/java/Application.java
+++ pluralsight-spring/0.2. conference-xml/src/main/java/Application.java
@@ -1,9 +1,13 @@
 import com.pluralsight.service.SpeakerService;
 import com.pluralsight.service.SpeakerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Application {
     public static void main(String[] args) {
-        SpeakerService service = new SpeakerServiceImpl();
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        SpeakerService service = context.getBean("speakerService", SpeakerService.class);
 
         System.out.println(service.findAll().get(0).getFirstName());
     }
--- pluralsight-spring/0.2. conference-xml/src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
+++ pluralsight-spring/0.2. conference-xml/src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -1,18 +1,27 @@
 package com.pluralsight.service;
 
 import com.pluralsight.model.Speaker;
-import com.pluralsight.repository.HibernateSpeakerRepositoryImpl;
 import com.pluralsight.repository.SpeakerRepository;
 
 import java.util.List;
 
 public class SpeakerServiceImpl implements SpeakerService {
 
-    private SpeakerRepository repository = new HibernateSpeakerRepositoryImpl();
+    private SpeakerRepository repository;
+
+    public SpeakerServiceImpl() {
+    }
+
+    public SpeakerServiceImpl(SpeakerRepository repository) {
+        this.repository = repository;
+    }
 
     @Override
     public List<Speaker> findAll() {
         return repository.findAll();
     }
 
+    public void setSpeakerRepository(SpeakerRepository repository) {
+        this.repository = repository;
+    }
 }
